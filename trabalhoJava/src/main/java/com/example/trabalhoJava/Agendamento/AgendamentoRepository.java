93124306ae292c4591b444549d49c482ca69f2eb
Guilherme Campos <134101701+httpsguime@users.noreply.github.com>
2024-06-02 20:02:39 -0300

    Delete trabalhoJava directory

--- trabalhoJava/src/main/java/com/example/trabalhoJava/Agendamento/AgendamentoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.trabalhoJava.Agendamento;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
-}
